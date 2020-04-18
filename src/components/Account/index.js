c5ed43117abfbca9f746c5079232e846d32de854
Daniel Barenboim <barendan@gmail.com>
2020-04-18 10:17:59 -0400

    backup

---
+++ src/components/Account/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { withAuthorization, AuthUserContext } from '../Session';
+import { PasswordForgetForm } from '../PasswordForget';
+import PasswordChangeForm from '../PasswordChange';
+
+
+const AccountPage = () => (
+	<AuthUserContext.Consumer>
+		{authUser => (
+			<div>
+				<h1>Account {authUser.email}</h1>
+				<PasswordForgetForm />
+				<PasswordChangeForm />
+			</div>
+		)}
+	</AuthUserContext.Consumer>
+)
+
+const condition = authUser => !!authUser;
+
+// const condition = authUser =>
+// 	authUser && !!authUser.roles[ROLES.ADMIN];
+
+export default withAuthorization(condition)(AccountPage);
